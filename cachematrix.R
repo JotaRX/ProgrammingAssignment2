7c95471f057ec17f3761aec3f3911f2d40dd56b8
JotaRX <jjrlhy@gmail.com>
2020-07-18 13:50:02 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignament 2
+## The objective that this functions is create a matrix, save it in the caché and
+## then calculate his inverse without use a lot of resources
 
-## Write a short comment describing this function
+## Set matrix and clear his inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i<-NULL
+  setmatrix<-function(y){
+    x<<-y
+    i<<-NULL
+  }
+  getmatrix <- function() x
+  setinv <- function(inv) i<<- inv
+  getinv <- function() i
+  list(setmatrix = setmatrix, getmatrix = getmatrix,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Verify the inverse previous and if this exist getting it of cache, else then 
+## calculate his inverse and set it in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i<-x$getinv()
+  if(!is.null(i)){
+    message("Obtained Matrix from cache")
+    return(i)
+  }
+  i<-x$getmatrix()
+  inv<-solve(i)
+  x$setinv(inv)
+  inv
 }
+
+#For example if you want you can delete the # and test with this randomly matrix 3x3
+
+#matriz<-makeCacheMatrix(matrix(rnorm(9),3,3))
+#cacheSolve(matriz)
